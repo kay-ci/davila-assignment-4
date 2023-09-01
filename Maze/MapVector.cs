10d27dbf2481868c17f118152b9f7f255de7ff31
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-09-01 10:59:41 -0400

    Added overloaded operator funtions to MapVector, tested

--- Maze/MapVector.cs
+++ Maze/MapVector.cs
@@ -13,6 +13,11 @@ namespace Maze
         public int X { get; set; }
 
         public int Y { get; set; }
+        public MapVector(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
 
         public bool InsideBoundary(int width, int height)
         {
@@ -21,7 +26,35 @@ namespace Maze
 
         public double Magnitude()
         {
-            throw new NotImplementedException();
+            double magnitude = Math.Sqrt(X * X + Y * Y);
+            return Math.Round(magnitude,2);
         }
+
+        public static implicit operator MapVector(int x)
+        {
+            return new MapVector((int)x, (int)x);
+        }
+
+        public static MapVector operator +(MapVector v1, MapVector v2) 
+        { 
+            int x = v1.X + v2.X;
+            int y = v1.Y + v2.Y;
+            return new MapVector(x,y);
+        }
+
+        public static MapVector operator -(MapVector v1, MapVector v2)
+        {
+            int newX = v1.X - v2.X;
+            int newY = v1.Y - v2.Y;
+            return new MapVector(newX, newY);
+        }
+
+        public static MapVector operator *(MapVector v1, MapVector v2)
+        {
+            int newX = v1.X * v2.X;
+            int newY = v1.Y * v2.Y;
+            return new MapVector(newX, newY);
+        }
+        
     }
 }
---
+++ MazeTest/MapVectorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maze;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Tests
+{
+    [TestClass()]
+    public class MapVectorTests
+    {
+        [TestMethod()]
+        public void MapVectorTest()
+        {
+            MapVector Mv = new MapVector(2, 4);
+            Assert.AreEqual(2, Mv.X);
+            Assert.AreEqual(4, Mv.Y);
+        }
+
+        [TestMethod()]
+        public void InsideBoundaryTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void MagnitudeTest()
+        {
+            MapVector Mv = new MapVector(2,4);
+            double magnitude = Mv.Magnitude();
+
+            Assert.AreEqual(4.47, magnitude);
+        }
+        public void PlusOperationTest()
+        {
+            //adding constant plus vectorMap
+            MapVector Mv = new MapVector(2, 4);
+            var addedMv = Mv + 2;
+            MapVector ExpectedMv = new MapVector(4, 6);
+
+            Assert.AreEqual(ExpectedMv, addedMv);
+
+            //adding 2 VectorMaps 
+            MapVector vectors = Mv + ExpectedMv;
+        }
+    }
+}
