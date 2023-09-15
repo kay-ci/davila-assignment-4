6d295c552e62d57e120f9330d6b68d010b1cc227
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-09-15 13:39:31 -0400

    Added played test methods

--- MazeTest/PlayerTests.cs
+++ MazeTest/PlayerTests.cs
@@ -11,15 +11,26 @@ namespace Maze.Tests
     [TestClass()]
     public class PlayerTests
     {
+        public Block[,] mapGrid { get; set; }
+        [TestInitialize]
+        public void Init()
+        {
+            mapGrid = new Block[5, 5]{
+                {Block.Solid,Block.Solid,Block.Solid,Block.Solid,Block.Solid },
+                {Block.Solid,Block.Empty,Block.Empty,Block.Empty, Block.Solid},
+                {Block.Solid,Block.Empty,Block.Solid, Block.Empty,Block.Solid},
+                {Block.Solid,Block.Empty,Block.Solid, Block.Empty,Block.Solid},
+                {Block.Solid,Block.Solid,Block.Solid,Block.Solid,Block.Solid},
+            };
+        }
         [TestMethod()]
         public void PlayerTest()
         {
             //Arrange
-            Block[,] mapGrid = new Block[5,5];
             int startX = 1;
             int startY = 3;
             //Act
-            var testPlayer = new Player(startX,startY,mapGrid);
+            var testPlayer = new Player(startX, startY, mapGrid);
             //Assert
             Assert.AreEqual(1, testPlayer.Position.X);
         }
@@ -28,50 +39,115 @@ namespace Maze.Tests
         public void GetRotationTest()
         {
             //Arrange
+            int startX = 1;
+            int startY = 1;
+            Player player = new Player(startX, startY, mapGrid);
 
             //Act
+            float northR = player.GetRotation();
+
+            player.TurnRight();
+            float eastR = player.GetRotation();
+
+            player.TurnRight();
+            float southR = player.GetRotation();
+
+            player.TurnRight();
+            float westR = player.GetRotation();
 
             //Assert
+            Assert.AreEqual(0f, northR, 0.01);
+            Assert.AreEqual(1.571f, eastR, 0.01);
+            Assert.AreEqual(3.141f, southR, 0.01);
+            Assert.AreEqual(4.712f, westR, 0.01);
         }
 
         [TestMethod()]
         public void MoveBackwardTest()
         {
             //Arrange
+            int startX = 1;
+            int startY = 1;
+            Player player = new Player(startX, startY, mapGrid);
 
             //Act
+            player.MoveBackward();
 
             //Assert
+            Assert.AreEqual(1, player.Position.X);
+            Assert.AreEqual(2, player.Position.Y);
         }
 
         [TestMethod()]
         public void MoveForwardTest()
         {
             //Arrange
+            int startX = 1;
+            int startY = 3;
+            Player player = new Player(startX, startY, mapGrid);
 
             //Act
+            player.MoveForward();
 
             //Assert
+            Assert.AreEqual(1, player.Position.X);
+            Assert.AreEqual(2, player.Position.Y);
         }
 
         [TestMethod()]
         public void TurnLeftTest()
         {
             //Arrange
+            int startX = 1;
+            int startY = 3;
+            Player player = new Player(startX, startY, mapGrid);
 
             //Act
+            player.TurnLeft();
+            Direction turn1 = player.Facing;
+
+            player.TurnLeft();
+            Direction turn2 = player.Facing;
+
+            player.TurnLeft();
+            Direction turn3 = player.Facing;
+
+            player.TurnLeft();
+            Direction turn4 = player.Facing;
 
             //Assert
+            Assert.AreEqual(Direction.W, turn1);
+            Assert.AreEqual(Direction.S, turn2);
+            Assert.AreEqual(Direction.E, turn3);
+            Assert.AreEqual(Direction.N, turn4);
         }
 
         [TestMethod()]
         public void TurnRightTest()
         {
             //Arrange
+            int startX = 1;
+            int startY = 3;
+            Player player = new Player(startX, startY, mapGrid);
 
             //Act
+            player.TurnRight();
+            Direction turn1 = player.Facing;
+
+            player.TurnRight();
+            Direction turn2 = player.Facing;
+
+            player.TurnRight();
+            Direction turn3 = player.Facing;
+
+            player.TurnRight();
+            Direction turn4 = player.Facing;
 
             //Assert
+            Assert.AreEqual(Direction.E, turn1);
+            Assert.AreEqual(Direction.S, turn2);
+            Assert.AreEqual(Direction.W, turn3);
+            Assert.AreEqual(Direction.N, turn4);
         }
     }
 }
