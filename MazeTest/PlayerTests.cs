d3f6adc37a2f6cb2535bf4e3e33335f7edb7d690
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-09-15 05:28:42 -0400

    Added player tests

---
+++ MazeTest/PlayerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maze;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void PlayerTest()
+        {
+            //Arrange
+            Block[,] mapGrid = new Block[5,5];
+            int startX = 1;
+            int startY = 3;
+            //Act
+            var testPlayer = new Player(startX,startY,mapGrid);
+            //Assert
+            Assert.AreEqual(1, testPlayer.Position.X);
+        }
+
+        [TestMethod()]
+        public void GetRotationTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod()]
+        public void MoveBackwardTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod()]
+        public void MoveForwardTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod()]
+        public void TurnLeftTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod()]
+        public void TurnRightTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
