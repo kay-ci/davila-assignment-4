3796ce266ea7698fc64fce2bca53d8464a75ab78
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-10-04 17:49:41 -0400

    Test folder, fix dependencies

--- MazeGame/Program.cs
+++ MazeGame/Program.cs
@@ -3,34 +3,34 @@ using System.Windows.Forms;
 public class Program{
     public static void Main(string[] args)
     {
-        //using var game = new MazeGame.Game1();
-        //game.Run();
+        using var game = new MazeGame.Game1();
+        game.Run();
 
-        var fileContent = string.Empty;
-        var filePath = string.Empty;
-        using (OpenFileDialog openFileDialog = new OpenFileDialog())
-        {
-            openFileDialog.InitialDirectory = "c:\\davila-assignement-2";
-            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileDialog.FilterIndex = 2;
-            openFileDialog.RestoreDirectory = true;
+        //var fileContent = string.Empty;
+        //var filePath = string.Empty;
+        //using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        //{
+        //    openFileDialog.InitialDirectory = "c:\\davila-assignement-2";
+        //    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+        //    openFileDialog.FilterIndex = 2;
+        //    openFileDialog.RestoreDirectory = true;
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                //Get the path of specified file
-                filePath = openFileDialog.FileName;
+        //    if (openFileDialog.ShowDialog() == DialogResult.OK)
+        //    {
+        //        //Get the path of specified file
+        //        filePath = openFileDialog.FileName;
 
-                //Read the contents of the file into a stream
-                var fileStream = openFileDialog.OpenFile();
+        //        //Read the contents of the file into a stream
+        //        var fileStream = openFileDialog.OpenFile();
 
-                using (StreamReader reader = new StreamReader(fileStream))
-                {
-                    fileContent = reader.ReadToEnd();
-                }
-            }
-        }
+        //        using (StreamReader reader = new StreamReader(fileStream))
+        //        {
+        //            fileContent = reader.ReadToEnd();
+        //        }
+        //    }
+        //}
 
-        MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
     }
 }
 
