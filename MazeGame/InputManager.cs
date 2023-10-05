0878d43577c11ab8c6f430a2c0603d67e1816c2e
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-10-05 12:46:24 -0400

    Modified singleton pattern

--- MazeGame/InputManager.cs
+++ MazeGame/InputManager.cs
@@ -10,29 +10,25 @@ namespace MazeGame
 {
     public class InputManager
     { 
-        private static readonly InputManager _instance = new InputManager();
-
-        public Action<Keys> HandleKey = (key) => 
-        {
-            Console.WriteLine($"test: {key}");
-        };
+        private static InputManager _instance = null;
         private InputManager(){}
         static InputManager(){}
 
         public static InputManager Instance
         {
-            get
-            {
-                return _instance;
-            }
+            get{
+                if (_instance == null) {
+                    _instance = new InputManager();
+                }
+                return _instance;}
         }
         public void AddKeyHandler(Keys key, Action action)
         {
 
         }
-        public void MoveForward() { }
-        public void MoveBackwards() { }
-        public void TurnLeft() { }
-        public void TurnRight() { }
+        public void Update()
+        {
+
+        }
     }
 }
