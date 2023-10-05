ecc4a576bc68325d78db1e62bc30db67850552ed
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-10-05 00:56:43 -0400

    Player Only drawn when moved, redraw path (buggy)

--- MazeGame/MazeGame.cs
+++ MazeGame/MazeGame.cs
@@ -12,7 +12,6 @@ public class MazeGame : Game
     public Map _map;
     private SpriteBatch _spriteBatch;
     private Texture2D _pathTexture;
-    private Texture2D _bunnyTexture;
     private Texture2D _goalTexture;
     private Texture2D _solidTexture;
     
@@ -31,7 +30,7 @@ public class MazeGame : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        string filePath = "C:\\Users\\kayci\\School\\Programming_V\\davila-assignement-2\\map9x13.txt";
+        string filePath = "C:\\Users\\kayci\\School\\Programming_V\\davila-assignement-2\\map9x7.txt";
         _map = new Map(new MazeFromFile.MazeFromFile(filePath));
         _map.CreateMap();
         _player = new PlayerSprite(this, _map);
@@ -42,7 +41,6 @@ public class MazeGame : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        _bunnyTexture = Content.Load<Texture2D>("bunny");
         _goalTexture = Content.Load<Texture2D>("goal");
         _pathTexture = Content.Load<Texture2D>("path");
         _solidTexture = Content.Load<Texture2D>("solid");
@@ -56,17 +54,16 @@ public class MazeGame : Game
         // TODO: Add your update logic here
 
         base.Update(gameTime);
-        _isMazeGenerated = false;
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
         int pixels = 32;
         _spriteBatch.Begin();
         // TODO: Add your drawing code here
         if(!_isMazeGenerated)
         {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
             _graphics.PreferredBackBufferHeight = _map.Height * pixels;
             _graphics.PreferredBackBufferWidth = _map.Width * pixels;
             _graphics.ApplyChanges();
@@ -88,6 +85,7 @@ public class MazeGame : Game
             }
             _spriteBatch.Draw(_goalTexture, new Vector2(_map.Goal.X * pixels, _map.Goal.Y * pixels), new Rectangle(0, 0, pixels, pixels), Color.White);
         }
+        _isMazeGenerated = true;
         _spriteBatch.End(); 
         base.Draw(gameTime);
     }
--- MazeGame/PlayerSprite.cs
+++ MazeGame/PlayerSprite.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +18,8 @@ namespace MazeGame
         private Game _game;
         private SpriteBatch _spriteBatch;
         private Texture2D _playerTexture;
+        private Texture2D _pathTexture;
         private Vector2 _previousPosition;
-        private Vector2 _position;
         private KeyboardState _previousState;
         public PlayerSprite(Game game, Map map) : base(game)
         {
@@ -27,7 +28,7 @@ namespace MazeGame
         }
         public override void Initialize()
         {
-            _position = new Vector2(_map.Player.StartX * Pixels, _map.Player.StartY * Pixels);
+            _previousPosition = new Vector2(_map.Player.StartX * Pixels, _map.Player.StartY * Pixels);
             base.Initialize();
         }
 
@@ -35,6 +36,7 @@ namespace MazeGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _playerTexture = _game.Content.Load<Texture2D>("bunny");
+            _pathTexture = _game.Content.Load<Texture2D>("path");
             base.LoadContent();
         }
 
@@ -51,10 +53,14 @@ namespace MazeGame
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Down) & !_previousState.IsKeyDown(Keys.Down))
             {
+                _previousPosition.X = _map.Player.Position.X * Pixels;
+                _previousPosition.Y = _map.Player.Position.Y * Pixels;
                 _map.Player.MoveForward();
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Up) & !_previousState.IsKeyDown(Keys.Up))
             {
+                _previousPosition.X = _map.Player.Position.X * Pixels;
+                _previousPosition.Y = _map.Player.Position.Y * Pixels;
                 _map.Player.MoveBackward();
             }
             if (_map.Player.Position.Equals(_map.Goal))
@@ -68,11 +74,24 @@ namespace MazeGame
 
         public override void Draw(GameTime gameTime)
         {
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(_playerTexture, new Vector2(_map.Player.Position.X * Pixels, _map.Player.Position.Y * Pixels), new Rectangle(0,0,Pixels,Pixels), 
-                Color.White, _map.Player.GetRotation(), new Vector2(_playerTexture.Width/2, _playerTexture.Height/2), 1, SpriteEffects.None, 0);
-            _previousPosition = _position;
-            _spriteBatch.End();
+            if (_previousPosition.X != _map.Player.Position.X  && _previousPosition.Y != _map.Player.Position.Y)
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(
+                    _playerTexture, 
+                    new Vector2(_map.Player.Position.X * Pixels, _map.Player.Position.Y * Pixels),
+                    new Rectangle(0, 0, Pixels, Pixels),
+                    Color.White, 
+                    _map.Player.GetRotation(),
+                    new Vector2(_playerTexture.Width / 2, _playerTexture.Height / 2), 
+                    1,
+                    SpriteEffects.None,
+                    0);
+
+                _spriteBatch.Draw(_pathTexture, _previousPosition, new Rectangle(0, 0, Pixels, Pixels), Color.White);
+                _spriteBatch.End();
+            }
+            
             base.Draw(gameTime);
         }
 
