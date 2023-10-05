d4f05bc554a1a3e24b1852072257bfc0266a817d
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-10-05 13:04:22 -0400

    Attempting input manager

--- MazeGame/InputManager.cs
+++ MazeGame/InputManager.cs
@@ -11,7 +11,10 @@ namespace MazeGame
     public class InputManager
     { 
         private static InputManager _instance = null;
-        private InputManager(){}
+        private Dictionary<Keys, List<Action>> _keysHandler;
+        private InputManager(){
+            _keysHandler = new Dictionary<Keys, List<Action>>();
+        }
         static InputManager(){}
 
         public static InputManager Instance
@@ -22,9 +25,12 @@ namespace MazeGame
                 }
                 return _instance;}
         }
+        
         public void AddKeyHandler(Keys key, Action action)
         {
-
+            List<Action> actions = new List<Action>();
+            actions.Add(action);
+            _keysHandler.Add(key, actions);
         }
         public void Update()
         {
