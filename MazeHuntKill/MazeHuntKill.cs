050309171b0f7bcd97e0e3aa17e5587b76dc6ca7
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-16 01:07:49 -0500

    Almost completed the walking phase of the hunt and kill algorithm

--- MazeHuntKill/MazeHuntKill.cs
+++ MazeHuntKill/MazeHuntKill.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 public class MazeHuntKill : IMapProvider
 {
     private Random _random;
+    private bool[,]? _visitedArray;
+    private MapVector _currentPosition;
     public MazeHuntKill() 
     {
         _random = new Random();
@@ -16,31 +18,34 @@ public class MazeHuntKill : IMapProvider
 
         // Set appropriate size
         Direction[,] directionArray = new Direction[arrayHeight, arrayWidth];
+        _visitedArray = new bool[arrayHeight, arrayWidth];
 
         // Pick random starting vector
         int rWidth = _random.Next(0, arrayWidth);
         int rHeight = _random.Next(0, arrayHeight);
-        MapVector currentPosition = new(rWidth, rHeight);
+        _currentPosition = new(rWidth, rHeight);
         do
         {
             // Walking phase here
+            _visitedArray[_currentPosition.Y, _currentPosition.X] = true;
+
             // Choose a random valid Direction 
-            Direction[] validDirections = getValidDirections(directionArray, currentPosition);
+            Direction[] validDirections = getValidDirections(directionArray);
             Direction randomValidDir = validDirections[_random.Next(0, validDirections.Length)];
 
             // Next position info
-            MapVector nextPosition = randomValidDir + currentPosition ;
+            MapVector nextPosition = randomValidDir + _currentPosition ;
             Direction oppositeDir = GetOppositeDirection(randomValidDir);
 
             // Update current position
-            directionArray[currentPosition.Y, currentPosition.X] = directionArray[currentPosition.Y, currentPosition.X]  | randomValidDir;
+            directionArray[_currentPosition.Y, _currentPosition.X] = directionArray[_currentPosition.Y, _currentPosition.X]  | randomValidDir;
 
             // Update next position 
             directionArray[nextPosition.Y, nextPosition.X] = directionArray[nextPosition.Y, nextPosition.X] | oppositeDir;
 
             // Update current position 
-            currentPosition = nextPosition;
-        } while (!validPosition(directionArray, currentPosition));
+            _currentPosition = nextPosition;
+        } while (!ValidPosition(directionArray, _currentPosition));
 
         // Hunting phase here
 
@@ -70,24 +75,48 @@ public class MazeHuntKill : IMapProvider
         }
         return newDir;
     }
-    private bool validPosition(Direction[,] directionArray, MapVector currentPosition)
+    private bool ValidPosition(Direction[,] directionArray, MapVector nextPosition)
     {
-        MapVector nextPosition = new(1, 1);
-        return true;
+        if (_visitedArray != null)
+        {
+            if (_visitedArray[nextPosition.Y, nextPosition.X] == false)
+            {
+                if (nextPosition.InsideBoundary(directionArray.GetLength(1), directionArray.GetLength(0)) && IsAdjacent(nextPosition))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    private Direction[] getValidDirections(Direction[,] directionArray, MapVector currentPosition)
+    private Direction[] getValidDirections(Direction[,] directionArray)
     {
-        // Get all the possible directions you can walk to for the given position You can walk to a new position if:
+        var directions = new Direction[] { Direction.S, Direction.W, Direction.N, Direction.E };
+        var possibleDirection = new List<Direction>();
 
-        // i.It has not been touched
+        foreach (var dir in directions)
+        {
+            MapVector nextPosition = dir + _currentPosition;
+            if (ValidPosition(directionArray, nextPosition))
+            {
+                possibleDirection.Add(dir);
+            }
 
-        // ii.It is inside the map
-
-        // iii.It is immediately adjacent to your current position
-        throw new NotImplementedException();
+        }
+        return possibleDirection.ToArray();
     }
 
+    // Check if the next position is adjacent to the _currentPosition
+    private bool IsAdjacent(MapVector nextPosition)
+    {
+        if (nextPosition + Direction.S == _currentPosition || nextPosition + Direction.N == _currentPosition ||
+            nextPosition + Direction.E == _currentPosition || nextPosition + Direction.W == _currentPosition)
+        {
+            return true;
+        }
+        return false;
+    }
     public Direction[,] CreateMap()
     {
         throw new NotImplementedException();
