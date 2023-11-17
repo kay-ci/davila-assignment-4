7b7bb5f6ff3fd6505fbf74d53c305a5a6e7b71ef
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-17 10:49:19 -0500

    Test public CreateMap with default values

--- MazeHuntKillTests/MazeHuntKillTests.cs
+++ MazeHuntKillTests/MazeHuntKillTests.cs
@@ -46,4 +46,30 @@ public class MazeRecursionTests
         Assert.AreEqual(Direction.E, dirArray[2, 2]);
         Assert.AreEqual(Direction.N | Direction.W, dirArray[2, 3]);
     }
+
+    [TestMethod()]
+    public void CreateMapTest()
+    {
+        // If Initial position is [0,0]
+        var mazeRecursion = new MazeHuntKill.MazeHuntKill(0);
+
+        // Test the CreateMap with default values 9x7
+        Direction[,] dirArray = mazeRecursion.CreateMap();
+        Assert.IsNotNull(dirArray);
+        Assert.AreEqual(Direction.S, dirArray[0, 0]);
+        Assert.AreEqual(Direction.E | Direction.S, dirArray[0, 1]);
+        Assert.AreEqual(Direction.E | Direction.S | Direction.W, dirArray[0, 2]);
+        Assert.AreEqual(Direction.W, dirArray[0, 3]);
+
+        Assert.AreEqual(Direction.N | Direction.S, dirArray[1, 0]);
+        Assert.AreEqual(Direction.N | Direction.S, dirArray[1, 1]);
+        Assert.AreEqual(Direction.N | Direction.E, dirArray[1, 2]);
+        Assert.AreEqual(Direction.S | Direction.W, dirArray[1, 3]);
+
+        Assert.AreEqual(Direction.N | Direction.E, dirArray[2, 0]);
+        Assert.AreEqual(Direction.N | Direction.W, dirArray[2, 1]);
+        Assert.AreEqual(Direction.E, dirArray[2, 2]);
+        Assert.AreEqual(Direction.N | Direction.W, dirArray[2, 3]);
+    }
+
 }
